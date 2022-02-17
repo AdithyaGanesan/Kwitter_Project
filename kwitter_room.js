de8a66dcbea3fb19d48a9aa3e59ea6c5fc21e060
AdithyaGanesan <82070286+AdithyaGanesan@users.noreply.github.com>
2022-02-17 17:49:01 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAPwY-RmLc1kOQ95es-sYtrAHYXdWHeTWs",
+    authDomain: "kwitter-78893.firebaseapp.com",
+    projectId: "kwitter-78893",
+    storageBucket: "kwitter-78893.appspot.com",
+    messagingSenderId: "173588472053",
+    appId: "1:173588472053:web:606de09220c2037845e5bc"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
